f2c596fd4e11f2e0e183ed2f21ece7db4c95eac0
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-18 09:17:57 -0800

    thorouhgly test on Order GetAll()

--- OrderTracker.Test/OrderTrackerTests.cs
+++ OrderTracker.Test/OrderTrackerTests.cs
@@ -6,13 +6,28 @@ using OrderTracker.Models;
 namespace OrderTracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests : IDisposable
   {
+    public void Dispose()
+    {
+      Order.ClearAll();
+    }
+
     [TestMethod]
     public void OrderConstructor_CreateNewInstanceOfOrder_Order()
     {
       Order newOrder = new Order("The Usual", "Local Coffee's usual order of day old croissant and Marionberry Danishes.", 100, "1/1/2020");
       Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
+
+    [TestMethod]
+    public void GetAll_RetriveStaticListOfAllOrderInstances_OrderList()
+    {
+      Order newOrder = new Order("The Usual", "Local Coffee's usual order of day old croissant and Marionberry Danishes.", 100, "1/1/2020");
+      List<Order> newList = new List<Order> {newOrder};
+      List<Order> result = Order.GetAll();
+      List<string> stringresult = new List<string>{};
+      CollectionAssert.AreEqual(newList, result);
     }
   }
 
--- OrderTracker/Models/Orders.cs
+++ OrderTracker/Models/Orders.cs
@@ -5,17 +5,31 @@ namespace OrderTracker.Models
 {
   public class Order
   {
+    //Properties
     public string Title{get;set;}
     public string Description{get;set;}
     public int Price{get;set;}
     public string Date{get;set;}
-
+    private static List<Order> _instances = new List<Order>{};
+    //Constructor
     public Order(string title, string description, int price, string date)
     {
       Title = title;
       Description = description;
       Price = price;
       Date = date;
+      _instances.Add(this);
+    }
+
+    public static List<Order> GetAll()
+    {
+      return _instances;
+    }
+
+    public static void ClearAll()
+    {
+      _instances.Clear();
     }
   }
+
 }
